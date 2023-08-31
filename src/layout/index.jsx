41cba04ab43985a88a07ccd39617659b4f1e092f
JesusRafaelNavaCruz <jnava@erillamhc.com>
2023-08-31 00:26:08 -0600

    create main layout, integrate sidebar and appbar

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,10 +1,66 @@
-//import { useMediaQuery, useTheme } from "@mui/material"
-//import { useSelector } from "react-redux";
+import { useState } from "react";
+import { AppBar, Box, Toolbar, useTheme, useMediaQuery, styled } from "@mui/material";
+import Navbar from "../components/common/Navbar";
+import Sidebar from "../components/common/Sidebar";
+
+const drawerWidth = '240'
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
+    ...theme.typography.mainContent,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    transition: theme.transitions.create(
+      'margin',
+      open
+        ? {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen
+          }
+        : {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen
+          }
+    ),
+    [theme.breakpoints.up('md')]: {
+      marginLeft: open ? 0 : -(drawerWidth - 20),
+      width: `calc(100% - ${drawerWidth}px)`
+    },
+    [theme.breakpoints.down('md')]: {
+      marginLeft: '20px',
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: '16px'
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '10px',
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: '16px',
+      marginRight: '10px'
+    }
+  }));
 
 const AdminLayout = () => {
-    //const theme = useTheme();
-    //const matchDownMd = useMediaQuery(theme.breakpoints.down("md"));
 
-}
+    const theme = useTheme();
+    const matchDownMd = useMediaQuery(theme.breakpoints.down('md'));
+    const [opened, setOpened] = useState(true);
 
-export default AdminLayout
+    const handleLeftDrawerToggle = () => {
+        setOpened(!opened);
+      };
+
+  return (
+    <Box sx={{ display: 'flex'}}>
+      <AppBar>
+        <Toolbar>
+          <Navbar handleLeftDrawerToggle={handleLeftDrawerToggle} />
+        </Toolbar>
+      </AppBar>
+      <Sidebar drawerOpen={!matchDownMd ? opened : !opened} drawerToggle={handleLeftDrawerToggle}/>
+      <Main theme={theme} open={opened}>
+
+      </Main>
+    </Box>
+  );
+};
+
+export default AdminLayout;
--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/prop-types */
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 // Components
-import Dashboard from "../pages/private/Dashboard"
+import Dashboard from "../pages/private/Dashboard";
 import Users from "../pages/private/management/Users";
 import Tables from "../pages/private/management/Tables";
 
+//Layout
+import AdminLayout from "../layout/index";
+
 const PrivateRoutes = () => {
   const auth = useAuth();
-  return auth && (
-    <Routes>  
-      <Route path="/dashboard" exact='true' element={ <Dashboard /> } />
-      <Route path="/users" exact='true' element={ <Users /> } />
-      <Route path="/tables" exact='true' element={ <Tables /> } />
-    </Routes>
-  )
-}
+  return (
+    auth && (
+      <AdminLayout>
+        <Routes>
+          <Route path="/dashboard" exact="true" element={<Dashboard />} />
+          <Route path="/users" exact="true" element={<Users />} />
+          <Route path="/tables" exact="true" element={<Tables />} />
+        </Routes>
+      </AdminLayout>
+    )
+  );
+};
 
 export default PrivateRoutes;
